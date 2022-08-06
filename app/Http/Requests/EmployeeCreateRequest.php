143efb4b6c45dfb982f032d3b88729803c2aed51
prabathchanaka <kasunprabathdpl@gmail.com>
2022-08-06 11:56:19 +0530

    added validation request class for employee create

--- app/Http/Requests/EmployeeCreateRequest.php
+++ app/Http/Requests/EmployeeCreateRequest.php
@@ -3,6 +3,7 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Carbon;
 
 class EmployeeCreateRequest extends FormRequest
 {
@@ -13,7 +14,7 @@ class EmployeeCreateRequest extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
@@ -23,8 +24,17 @@ class EmployeeCreateRequest extends FormRequest
      */
     public function rules()
     {
+        $dt = new Carbon();
+        $before18Years = $dt->subYears(18)->format('Y-m-d');
+
         return [
-            //
+            'first_name' => 'required|min:5',
+            'last_name' => 'required|min:5',
+            'gender' => 'required',
+            'date_of_birth' => 'required|date|before:' . $before18Years,
+            'hire_date' => 'required|date',
+            'designations' => 'array|min:1',
+            'salaries' => 'array|min:1',
         ];
     }
 }
